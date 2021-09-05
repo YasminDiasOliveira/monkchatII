dcc06c463a532ca1056511edc179dfdefd139e7c
Yasmin <yasmindias03@gmail.com>
2021-09-05 00:43:00 -0300

    okok

--- reactjs/src/pages/monkchat/conteudo.js
+++ reactjs/src/pages/monkchat/conteudo.js
@@ -29,6 +29,7 @@ export default function Conteudo() {
     const navigation = useHistory();
     let usuarioLogado = lerUsuarioLogado(navigation) || {};
 
+    const [idAlterando, setIdAlterando] = useState(0);
     const [chat, setChat] = useState([]);
     const [sala, setSala] = useState('');
     const [usu, setUsu] = useState(usuarioLogado.nm_usuario);
@@ -56,15 +57,26 @@ export default function Conteudo() {
     }
 
     const enviarMensagem = async (event) => {
-       if (event.type === "keypress" && (!event.ctrlKey || event.charCode !== 13
-        ))
-       return;
-
-        const resp = await api.inserirMensagem(sala, usu, msg);
-        if (!validarResposta(resp)) 
+       if (event.type === "keypress" && (!event.ctrlKey || event.charCode !== 13))
             return;
+
+        if (idAlterando > 0) {
+
+            const resp = await api.alterarMensagem(idAlterando, msg);
+            if (!validarResposta(resp)) 
+                return;
         
-        toast.dark('💕 Mensagem enviada com sucesso!');
+            toast.dark('💕 Mensagem alterada com sucesso!');
+            setIdAlterando(0);
+            setMsg('');
+
+        } else {
+            const resp = await api.inserirMensagem(sala, usu, msg);
+            if (!validarResposta(resp)) 
+                return;
+        
+            toast.dark('💕 Mensagem enviada com sucesso!');
+        }
         await carregarMensagens();
     }
 
@@ -94,6 +106,11 @@ export default function Conteudo() {
         
         toast.dark('💕 Mensagem removida!');
         await carregarMensagens();
+    }
+
+    const editar = async (item) => {
+        setMsg(item.ds_imagem);
+        setIdAlterando(item.id_chat);
     }
     
     return (
@@ -132,6 +149,7 @@ export default function Conteudo() {
                     {chat.map(x =>
                         <div key={x.id_chat}>
                             <div className="chat-message">
+                                <div> <img onClick={() => editar(x)} src="/assets/images/alterar.svg" alt="" style={{cursor: 'pointer'}} /> </div> 
                                 <div> <img onClick={() => remover(x.id_chat) } src="/assets/images/delete.svg" alt="" style={{cursor: 'pointer'}} /> </div> 
                                 <div>({new Date(x.dt_mensagem.replace('Z', '')).toLocaleTimeString()})</div>
                                 <div><b>{x.tb_usuario.nm_usuario}</b> fala para <b>Todos</b>:</div>
